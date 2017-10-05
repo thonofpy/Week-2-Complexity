56eda99cdc94060fd4b758b2f7957924f1762b35
MrNeutrino <32274898+MrNeutrino@users.noreply.github.com>
2017-10-05 18:24:21 +0100

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,36 +1,13 @@
+# This code is from Oswald.
+
 def sum_to_k(lst, k):
-	i, j = 0, 1
-	while j < len(lst):
+	i = 0
+	j = len(lst)-1
+	while i < j:
 		if lst[i] + lst[j] == k:
 			return True
-			break
-		i += 1
-		j += 1
-	else:
-		return False
-
-##################################
-# version 2 - might be bull shit #
-##################################
-def sum_to_k(lst, k):
-
-	# find middle of list
-	if len(lst) % 2 != 0:
-		middle = int(len(lst)/2)
-	else:
-		middle = int(len(lst)/2-1)
-
-	# if k < middle, start from lst[0], else start from middle
-	if k < lst[middle] + lst[middle+1]:
-		i, j = 0, 1
-	else:
-		i, j = middle, middle+1
-
-	while j < len(lst):
-		if lst[i] + lst[j] == k:
-			return True
-			break
-		i += 1
-		j += 1
-	else:
-		return False
+		elif lst[i] + lst[j] < k:
+			i += 1
+		else:
+			j -= 1
+	return False
